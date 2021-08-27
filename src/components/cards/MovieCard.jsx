6b121ce84ffe9cd20808a1411d673eaef9290a80
Luuchoh <luxo997@gmail.com>
2021-08-27 11:24:42 -0500

    popup detalle peli

--- src/components/cards/MovieCard.jsx
+++ src/components/cards/MovieCard.jsx
@@ -1,21 +1,27 @@
-import React, { memo } from 'react'
-import '../../style/styleMovieCard.css'
+import React, { Fragment, memo } from "react";
+import "../../style/styleMovieCard.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import MovieDetail from "./MovieDetail";
 
-const IMG_API = 'https://image.tmdb.org/t/p/w500/'
+const IMG_API = "https://image.tmdb.org/t/p/w500/";
 
-const MovieCard = memo(({
-    title,
-    poster_path,
-    vote_average
-}) => {
-    return (
-        <div className="card-movie">
-            
-                <div className="rating"><i style={{color: "#FED941"}} className="fas fa-star"></i>{" "}{vote_average}</div>
-                <img className="img-card" src={IMG_API + poster_path} alt="" /> 
-            
+const MovieCard = ({ title, poster_path, vote_average }) => {
+
+    // console.log(poster_path);
+  return (
+    <Fragment>
+      
+
+        <div className="rating">
+          <i style={{ color: "#FED941" }} className="fas fa-star"></i>{" "}
+          {vote_average}
         </div>
-    )
-})
+        <img className="img-card" src={IMG_API + poster_path} alt="" />
+      
+    </Fragment>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
---
+++ src/components/cards/MovieDetail.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Fragment } from "react";
+
+const MovieDetail = ({poster}) => {
+
+    console.log(poster);
+
+  return (
+    <Fragment>
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                <img src={poster} alt="movie-poster" />
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default MovieDetail;
