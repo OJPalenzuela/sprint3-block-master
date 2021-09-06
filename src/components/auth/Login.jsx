f20293f696863bbfd8763c0595fd553b5e024bbe
OJPalenzuela <palenzuelaomar@gmail.com>
2021-09-06 13:53:27 -0500

    Se mejaorae l diseño de auth

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import '../../style/styleLogin.css';
 import { useForm } from '../../hooks/useForm';
 import { useDispatch, useSelector } from 'react-redux';
